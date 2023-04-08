5444e4daf5f5a6212095900c9ad71fb77e584de2
mitaka1303 <mitaka1332@outlook.jp>
2023-04-08 19:12:11 +0900

    add_card_table

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import './components/ButtonAppBar'
 import ButtonAppBar from './components/ButtonAppBar'
+import BasicTable from './components/BasicTable'
+import BasicCard from './components/BasicCard'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
       <ButtonAppBar/>
+      <BasicCard/>
+      <BasicTable/>
     </div>
   )
 }
